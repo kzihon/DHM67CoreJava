aea18e6d5faf67a21203fd30b9e72d2c1e860620
Kidist Zihon <kidist@Kidists-MacBook-Pro.local>
2024-02-10 11:13:38 -0800

    mindering problem solved

--- src/TestForGit.java
+++ src/TestForGit.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TestForGit {
     public static void main(String[] args) {
         String[][] mat={{"c","a","t"},{"i","d","o"},{"n","o","m"}};
-        words(mat);
+       // words(mat);
+        int[] arr={-1,1,2,3,-5};
+        mindering(arr);
     }
     public static void words(String[][] mat){
         int row=mat.length;
@@ -29,5 +32,22 @@ public class TestForGit {
         System.out.println(str);
         System.out.println(str.contains("mom"));
         System.out.println(str.contains("cin"));
+    }
+    public static void mindering(int[] arr){
+        Arrays.sort(arr);
+        int mindering[]= new int[arr.length];
+        System.out.println(Arrays.toString(arr));
+        int i=0;
+        int j=arr.length-1;
+        int counter=0;
+        while (i<j){
+            mindering[counter++]=arr[j--];
+            mindering[counter++]=arr[i++];
+        }
+        if(arr.length%2!=0){
+            mindering[counter++]=arr[i];
+        }
+        System.out.println(Arrays.toString(mindering));
+
     }
 }
